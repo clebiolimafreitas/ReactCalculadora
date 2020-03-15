330f8cec199e6d69547482fd4f84cd11dcdfff4a
Clebio Lima de Freitas <clebiolima@gmail.com>
2020-03-15 16:37:13 -0300

    melhorias no cálculo e tratamento de erros.

--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -22,41 +22,54 @@ export default props => {
 
     const backSpace = _ => setDisplay(display.substring(0,display.length-1))
 
-    const myReplace = str => {
-        return str.replace(/%/g,'/100').replace(/÷/g,'/').replace(/x/g,'*')
-    }
+    const myReplace = str => str.replace(/%/g,'/100').replace(/÷/g,'/').replace(/x/g,'*')
 
-    const calcular = _ => {
-        setDisplay(eval(myReplace(memoria)).toString())        
+    const atualizaMemoriaCalculo = _ => {
+        //debugger
+        try{
+            if('+-x÷='.indexOf(tecla) < 0) {//numeros        
+                setDisplay(eval(myReplace(memoria)))           
+            }else if (tecla === "=" || '+-x÷'.indexOf(memoria.substring(memoria.length-2, memoria.length-1)) >= 0){ // pressionado igual(=) ou penúltima tecla foi um operador  
+                //criando regra para descobrir o indice do último operador digitado
+                const regex = new RegExp( /x|\+|\-|÷/, "g" )
+                regex.test(memoria.substring(0,memoria.length-2))
+                const indexUltimoOperador = regex.lastIndex
+                setTecla(memoria.substring(memoria.length-3,memoria.length-2)) //atualiza tecla atual para o último número digitado
+                // substitui o último operador digitado pelo novo
+                setMemoria(memoria.substring(0,memoria.length-2)+tecla+memoria.substring(indexUltimoOperador,memoria.length-2)) 
+            }else if ('+-x÷'.indexOf(tecla) >= 0){ //operadores
+                setDisplay(eval(myReplace(memoria.substring(0,memoria.length-1))))  
+            }
+        }catch(e){
+            inicializaVariaveis()
+            setDisplay("Error")
+            console.log(e)
+        }        
     }
      
     const atualizaDisplay = digito => { 
-        setTecla(String(digito))
-        switch (digito)
-        {           
-            case "C":
+        try{
+            setTecla(String(digito))
+            if (digito === "C"){
                 inicializaVariaveis()
-                break 
-            case "CE":
+            }else if (digito === "CE"){
                 inicializaDisplay()
-                break
-            case "←":
-                backSpace()
-                break
-            case "=":
-                calcular()     
-                break
-            default:  
-                if(memoria === "0")
-                    setMemoria(digito) 
-                else
-                    setMemoria(memoria+digito)      
-        }            
-    };
+            }else if (digito === "←"){
+                backSpace()       
+            }else if(digito !== "="){
+                setMemoria(memoria+digito)     
+            }else if(memoria === "0"){
+                setMemoria(digito)                    
+            }
+        }catch(e){
+            inicializaVariaveis()
+            setDisplay("Error")
+            console.log(e)
+        }        
+    }
 
-    useEffect(() => {        
-        if('%+-x÷'.indexOf(tecla) < 0)
-            calcular()           
+    useEffect(() => {    
+        atualizaMemoriaCalculo()           
     },[memoria])// eslint-disable-line
 
     const botoes = [
